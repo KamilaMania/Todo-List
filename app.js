d55eb86eeb24714b57058d8b7c6a26d88a379261
KamilaMania <mania.kamika@icloud.com>
2020-02-26 14:22:53 +0100

    deleting todos

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const addForm = document.querySelector(".add");
 const list = document.querySelector(".todos");
+
 const generateTemplate = todo => {
   const html = `<li class="list-group-item d-flex justify-content-between align-items-center">
     <span>${todo}</span>
@@ -15,5 +16,11 @@ addForm.addEventListener("submit", e => {
   if (todo.length) {
     generateTemplate(todo);
     addForm.reset();
+  }
+});
+
+list.addEventListener("click", e => {
+  if (e.target.classList.contains("delete")) {
+    e.target.parentElement.remove();
   }
 });
